eff0b134bda602446b0cd2f5845664f23420581a
Joseph A <joseph.atureta@gmail.com>
2022-02-13 13:36:23 +0100

    Changed next.config.js

--- next.config.js
+++ next.config.js
@@ -4,8 +4,4 @@ module.exports = {
   images: {
     domains: ['links.papareact.com', 'jsonkeeper.com/', 'upload.wikimedia.org'],
   },
-  env: {
-    mapbox_token:
-      'pk.eyJ1IjoidHVyZXgwNCIsImEiOiJja3pmeTBuM2UwdzBsMndwanF0dXNoZHUxIn0.5D4CaU4s5tyFjcfXAKZ0Tw',
-  },
 }
